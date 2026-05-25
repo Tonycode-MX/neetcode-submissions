494f73bb34411d7bf23064d3f0c3b01f8205ab7b
Anthony Martínez <93753023+Tonycode-MX@users.noreply.github.com>
2026-05-24 18:21:44 -0600

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+
+        windowLen = len(s1)
+
+        if windowLen > len(s2):
+            return False
+        
+        s1_letters = [0]*26
+        for letter in s1:
+            index = ord(letter) - ord("a")
+            s1_letters[index] += 1
+
+        window_letters = [0]*26
+        for letter in s2[:windowLen-1]:
+            index = ord(letter) - ord("a")
+            window_letters[index] += 1
+
+        for window in range(len(s2)-windowLen+1):
+            newLetter = ord(s2[window+windowLen-1]) - ord("a")
+            window_letters[newLetter] += 1
+
+            if window_letters == s1_letters:
+                return True
+
+            first_letter = ord(s2[window]) - ord("a")
+            window_letters[first_letter] -= 1
+
+        return False
+
+
+        # [1,2,3,4,5,6,7,8,9,0]
+        # 2
+        
